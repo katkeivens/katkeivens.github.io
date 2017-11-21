fc8bc835b1e213f493b3951167534678a7070c6f
katkeivens <katkeivens@gmail.com>
2017-11-21 00:50:40 -0800

    updated javascript reset

--- Homework_4/Crystals-Collector/assets/javascript/game.js
+++ Homework_4/Crystals-Collector/assets/javascript/game.js
@@ -2,34 +2,14 @@
 
 // global variables 
 
-var randomNumber, blueCrystal, greenCrystal, redCrystal, purpleCrystal;
+var blueCrystal = Math.floor((Math.random() * 12) + 1);
+var greenCrystal = Math.floor((Math.random() * 12) + 1);;
+var redCrystal = Math.floor((Math.random() * 12) + 1);;
+var purpleCrystal = Math.floor((Math.random() * 12) + 1);;
 var totalScore = 0;
 var wins = 0;
 var losses = 0;
 var message;
-
-
-function resetGame() {
-	// clear previous data from game prior
-	$('#totalScore').html(totalScore = 0);
-	//$('#totalScore').html(totalScore);
-	console.log(totalScore)
-	
-	randomNumber = Math.floor((Math.random() * 120) + 19);
-	blueCrystal = Math.floor((Math.random() * 12) + 1);
-	greenCrystal = Math.floor((Math.random() * 12) + 1);
-	redCrystal = Math.floor((Math.random() * 12) + 1);
-	purpleCrystal = Math.floor((Math.random() * 12) + 1);
-	
-	console.log('blue ' + blueCrystal)
-	console.log('green ' + greenCrystal)
-	console.log('red ' + redCrystal)
-	console.log('purple ' + purpleCrystal)
-}
-
-function resetMessages() {
-	$('#message').text('');
-}
 
 function win() {
 	$('#message').html('You won!  Play again!');
@@ -45,13 +25,43 @@ function lose() {
 	startGame();
 }
 
+function resetMessages() {
+	$('#message').text('');
+}
+
+function resetGame() {
+	// clear previous data from game prior
+	$('#totalScore').html(totalScore = 0);
+
+	$('.blue-gem').empty().off('click');
+	blueCrystal = Math.floor((Math.random() * 12) + 1);
+	$('.blue-gem').data(blueCrystal);
+
+	$('.green-gem').empty().off('click');
+	greenCrystal = Math.floor((Math.random() * 12) + 1);
+	$('.greenCrystal').data(greenCrystal);
+
+	$('.red-gem').empty().off('click');
+	redCrystal = Math.floor((Math.random() * 12) + 1);
+	$('.red-gem').data(redCrystal);
+
+	$('.purple-gem').empty().off('click');
+	purpleCrystal = Math.floor((Math.random() * 12) + 1);
+	$('.purple-gem').data(purpleCrystal);
+
+	console.log('blue ' + blueCrystal)
+	console.log('green ' + greenCrystal)
+	console.log('red ' + redCrystal)
+	console.log('purple ' + purpleCrystal)
+}
+
 function startGame() {
-	resetGame();
+	this.resetGame();
 	setTimeout('resetMessages()', 3000);
 
 	// Generate a random number
+	var randomNumber = Math.floor((Math.random() * 120) + 19);
 	$('#random-number').html(randomNumber);
-
 
 	// Assigning numbers to crystals
 	$('.blue-gem').on('click', function() {
